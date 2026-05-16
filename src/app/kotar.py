8915d7725d9443d736e0b91db2fc73bcb1299d61
antissimo <boskovicante1@gmail.com>
2026-05-16 13:42:11 +0200

    Dodani ispisi

--- src/app/kotar.py
+++ src/app/kotar.py
@@ -2,23 +2,27 @@ from enum import Enum
 
 
 class Kotar(Enum):
-    CENTAR = 1
-    VAROS = 2
-    MEJE = 3
-    SPINUT = 4
-    LOVRET = 5
-    VISOKA = 6
-    SUCIDAR = 7
-    SKALICE = 8
-    MEJASI = 9
-    STOBREC = 10
-    SOLIN = 11
-    KASTELA = 12
-    PODSTRANA = 13
-    NEPOZNATO = 99
+    CENTAR = (1, "Centar")
+    VAROS = (2, "Varoš")
+    MEJE = (3, "Meje")
+    SPINUT = (4, "Spinut")
+    LOVRET = (5, "Lovret")
+    VISOKA = (6, "Visoka")
+    SUCIDAR = (7, "Sućidar")
+    SKALICE = (8, "Skalice")
+    MEJASI = (9, "Mejaši")
+    STOBREC = (10, "Stobreč")
+    SOLIN = (11, "Solin")
+    KASTELA = (12, "Kaštela")
+    PODSTRANA = (13, "Podstrana")
+    NEPOZNATO = (99, "Nepoznato")
+
+    def __init__(self, broj, naziv):
+        self.broj = broj
+        self.naziv = naziv
 
     @staticmethod
     def ispisi():
         print("=== KOTARI ===")
         for kotar in Kotar:
-            print(f"{kotar.value} - {kotar.name}")
+            print(f"{kotar.broj} - {kotar.naziv}")
--- src/app/sluzba.py
+++ src/app/sluzba.py
@@ -1,23 +1,29 @@
 from enum import Enum
 
+
 class Sluzba(Enum):
-    POLICIJA = 1
-    VATROGASCI = 2
-    CISTOCA = 3
-    KOMUNALNO_REDARSTVO = 4
-    ZELENILO = 5
-    VODOVOD_KANALIZACIJA = 6
-    ODRZAVANJE_CESTA = 7
-    PROMET = 8
-    JAVNI_PRIJEVOZ = 9
-    JAVNA_RASVJETA = 10
-    GRADJEVINSKI_NADZOR = 11
-    HITNA_MEDICINA = 12
-    GRADSKA_UPRAVA = 13
-    NEPOZNATO = 99
+    POLICIJA = (1, "Policija")
+    VATROGASCI = (2, "Vatrogasci")
+    CISTOCA = (3, "Čistoća")
+    KOMUNALNO_REDARSTVO = (4, "Komunalno redarstvo")
+    ZELENILO = (5, "Zelenilo")
+    VODOVOD_KANALIZACIJA = (6, "Vodovod i kanalizacija")
+    ODRZAVANJE_CESTA = (7, "Održavanje cesta")
+    PROMET = (8, "Promet")
+    JAVNI_PRIJEVOZ = (9, "Javni prijevoz")
+    JAVNA_RASVJETA = (10, "Javna rasvjeta")
+    GRADJEVINSKI_NADZOR = (11, "Građevinski nadzor")
+    HITNA_MEDICINA = (12, "Hitna medicina")
+    GRADSKA_UPRAVA = (13, "Gradska uprava")
+    NEPOZNATO = (99, "Nepoznato")
+
+    def __init__(self, broj, naziv):
+        self.broj = broj
+        self.naziv = naziv
 
     @staticmethod
     def ispisi():
         print("=== SLUŽBE ===")
+
         for sluzba in Sluzba:
-            print(f"{sluzba.value} - {sluzba.name}")
+            print(f"{sluzba.broj} - {sluzba.naziv}")
